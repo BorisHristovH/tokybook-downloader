7131205a60777380ae57b0d0ef3c67cfd8d0b646
hristobo <boris.hristov@student.uni-augsburg.de>
2024-01-02 13:36:28 +0100

    first working basic downloading of whole book

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import sys
 import re
-import json
+import ast
 import requests
+import os
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup as bs
 
 BASE_PATH = './downloaded/'
+AUDIO_BOOK_NAME = 'A Feast for Crows'
+AUDIO_BOOK_PATH = BASE_PATH + AUDIO_BOOK_NAME + '/'
 AUDIO_FILE_BASE_URL = 'https://files02.tokybook.com/audio/'
 MP3_LINK_GETTER = 'https://api.crystallization.tv/api-us/getMp3Link'
+
 def extract_and_download_tracks_from_url():
     main_page_url = sys.argv[1]
     response_body = requests.get(main_page_url)
-    write_to_file("site.html", response_body.text)
+    #write_to_file("site.html", response_body.text)
     playlist = get_playlist_from_body(response_body)
 
     for track in playlist:
@@ -19,30 +23,30 @@ def extract_and_download_tracks_from_url():
         if track["name"] == 'welcome':
             continue
         mp3_name = track["name"] + ".mp3"
-        save_file_path = BASE_PATH + main_page_url.split('/')[-2] + '/' + mp3_name
+        save_file_path = AUDIO_BOOK_PATH + mp3_name
 
         uri = urlparse(AUDIO_FILE_BASE_URL)
         post_url = uri._replace(path=track_url_path).geturl()
 
         download_url = post_data_to_get_download_link(track)
-
+        print("Got the download url ", download_url)
         doc = requests.get(download_url)
 
-        with open('downloaded_song.mp3', 'wb') as file:
-                file.write(doc.content)
         write_to_file(save_file_path, doc.content)
+        print("Wrote to file ", save_file_path)
 
 
 # tracks are in a json in a specific script tag
 def get_playlist_from_body(page):
     soup = bs(page.content, "html.parser")
     playlist = soup.find(class_="entry-content")
-    scripts = list(map(lambda x:x.text, playlist.select("script")))
+    scripts = list(map(lambda x : str(x.contents), playlist.select("script")))
     the_script = list(filter(lambda a: 'tracks' in a, scripts))[0] # should be only one with tracks
 
     extracted_variable = re.findall(' tracks = (\[(?:\[??[^\[]*?\]))',the_script)[0]
     clean_json = clean_up_json(extracted_variable)
-    data = json.loads(clean_json) # sad not working
+    data = ast.literal_eval(clean_json)
+
     return data
 
 def post_data_to_get_download_link(track):
@@ -54,17 +58,24 @@ def post_data_to_get_download_link(track):
     return response.json()['link_mp3']
 
 def clean_up_json(extracted_variable):
-    return re.sub(r',\s*}', '}', extracted_variable)
+    formatted = re.sub(r',\s*}', '}', extracted_variable)
+    white_space_stripped = formatted.replace(" ", "").replace("\\n", "")
+    return white_space_stripped
+
 
 def download(track_url):
     pass
 
 
-def write_to_file(name, text):
-    f = open(name, 'w', encoding='utf-8')
-    f.write(text)
-    f.close()
+def write_to_file(name, content):
+    with open(name, "wb") as f:
+        f.write(content)
 
 
 if __name__ == "__main__":
+    isExist = os.path.exists(AUDIO_BOOK_PATH)
+    if not isExist:
+        # Create a new directory because it does not exist
+        os.makedirs(AUDIO_BOOK_PATH)
+        print("The ", AUDIO_BOOK_PATH, " directory is created!")
     extract_and_download_tracks_from_url()
